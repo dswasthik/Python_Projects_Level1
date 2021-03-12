ce4069a050241e61e38f1654538a31cf60dd2402
Swasthik D <43759020+dswasthik@users.noreply.github.com>
2021-03-12 20:46:18 +0530

    Add files via upload

---
+++ Colored Text/Colored Text.py
@@ -0,0 +1,6 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset = True)
+print(Fore.BLUE+Back.YELLOW+"Hello World,"+Fore.YELLOW+ Back.BLUE+"this is beautiful!")
+print(Back.CYAN+"Life is beautiful")
+print(Fore.RED + Back.GREEN+"Be good Do good")
